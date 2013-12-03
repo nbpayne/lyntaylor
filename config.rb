e50074d842fe62b1aedf581270be8223d2d0b6d9
Nick Payne <nick@nickpayne.net>
2013-12-03 21:10:43 +1100

    Update config.

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ add_import_path "bower_components/foundation/scss"
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "scss"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
@@ -17,9 +17,11 @@ javascripts_dir = "javascripts"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+output_style = :compressed # by Compass.app 
+line_comments = false # by Compass.app 
